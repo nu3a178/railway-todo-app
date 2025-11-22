080354733d58f21b1503b04e12c025958f0a0009
nu3a178 <nu197ma@gmail.com>
2025-11-22 10:47:07 +0900

    Revert "Reapply "vite server.proxyを設定""
    This reverts commit c075b07b43322959428fe2caa18456b548f2a868.

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    proxy: {
-      "/api": {
-        target: "http://railway.todo.techtrain.dev",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
   },
   resolve: {
     alias: {
-      "~": "/src",
+      '~': '/src',
     },
   },
-});
+})
