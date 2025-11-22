df32efe3be721ce2ee712c0bb111b292e1fac87f
nu3a178 <nu197ma@gmail.com>
2025-11-22 17:00:22 +0900

    ボタンをコンポーネント化 index.cssからボタンのcssの場所を移動した

---
+++ src/components/AppButton.jsx
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+import "./AppButton.css";
+import { useHistory } from "react-router-dom";
+
+export const AppButton = ({
+  label,
+  type = "button",
+  target,
+  color = "default",
+  onClick,
+}) => {
+  if (type === "submit") {
+    return (
+      <button type="submit" className={`app_button app_button_${color}`}>
+        {label}
+      </button>
+    );
+  }
+  if (target) {
+    return (
+      <Link className={`app_button app_button_${color}`} to={target}>
+        {label}
+      </Link>
+    );
+  }
+  return (
+    <button
+      type="button"
+      className={`app_button app_button_${color}`}
+      onClick={onClick}
+    >
+      {label}
+    </button>
+  );
+};
